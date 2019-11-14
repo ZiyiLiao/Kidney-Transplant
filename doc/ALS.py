7c75b5d8db0e30e079bf63bb75d7bfe226ccd8f7
ZiyiLiao <eziyi1996@icloud.com>
2019-11-14 14:54:40 -0500

    Update ALS

--- doc/ALS.py
+++ doc/ALS.py
@@ -1,3 +1,12 @@
+"""
+
+This is the file to output Absolute Least Square function
+Here using PySpark to perform
+
+"""
+
+# import packages
+
 import findspark
 findspark.init('/Users/ziyi./spark-2.4.4-bin-hadoop2.7')
 
@@ -7,11 +16,18 @@ from pyspark.sql import SparkSession
 from pyspark.ml.recommendation import ALS
 from pyspark.ml.evaluation import RegressionEvaluator
 import itertools
-from pyspark import SparkContext as sc
+from pyspark import SparkContext
 from functools import reduce 
 from pyspark.sql import DataFrame
+from pyspark.conf import SparkConf
 
-spark = SparkSession.builder.getOrCreate()
+# initiate spark
+spark = SparkSession.builder.enableHiveSupport().\
+    config('spark.executor.memory', '15g'). \
+    config('spark.executor.cores','20').  \
+    config('spark.executor.instances','20'). \
+    config('spark.driver.memory','20g'). \
+    getOrCreate()  
 
 # Data preparation
 
@@ -19,9 +35,30 @@ movies = pd.read_csv('movies.csv')
 df = pd.read_csv('ratings.csv')
 del df['timestamp']
 
-# small sample
-#df1 = df[0:400]
-ratings = spark.createDataFrame(df)
+
+# ratings = spark.createDataFrame(df)
+# ratings.show()
+
+print(df.shape)
+
+
+"""
+
+Calculate Sparsity
+
+"""
+numerator = ratings.select("rating").count()
+num_users = ratings.select("userId").distinct().count()
+num_movies = ratings.select("movieId").distinct().count()
+sparsity = (1.0 - (numerator *1.0)/(num_users * num_movies))*100
+print("The ratings dataframe is ", "%.2f" % sparsity + "% empty.")
+
+
+
+"""
+ALS Model
+
+"""
 
 
 # convert the columns to the proper data types
@@ -32,7 +69,7 @@ ratings = ratings.select(ratings.userId.cast("integer"),
 (training_data, test_data) = ratings.randomSplit([0.8, 0.2], seed=2019)
 
 # perform the cross validation to find a set of ideal tuning parameters
-def model_fit(r, m, l, k):
+def model_fit(r, m, l, k = 5):
     '''
     r: rank -> number of latent features
     m: maximum iterations
@@ -44,7 +81,7 @@ def model_fit(r, m, l, k):
     als = ALS(userCol = 'userId', itemCol = 'movieId',ratingCol = 'rating',
                 rank = r , maxIter = m, regParam = l,
                nonnegative = True, coldStartStrategy = 'drop',
-               implicitPrefs = True)
+               implicitPrefs = False)
     
     # evaluator
     evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating", predictionCol="prediction") 
@@ -78,10 +115,13 @@ def model_fit(r, m, l, k):
     return  min_cv_err, final_model
 
 
-
 ranks = [5]
 maxIters = [5]
 regParams = [0.1]
+
+# ranks = [5,10,20]
+# maxIters = [5, 50,100]
+# regParams = [0.01, 0.05, 0.1]
 
 results = {}
 for r, m, l in itertools.product(ranks, maxIters, regParams):
