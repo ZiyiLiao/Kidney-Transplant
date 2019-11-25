41bdf51edab7e22185b62e0b0d0f81a852a45757
ZiyiLiao <eziyi1996@icloud.com>
2019-11-25 16:29:15 -0500

    Update

--- lib/SGD.py
+++ lib/SGD.py
@@ -18,7 +18,7 @@ class SGD:
 
     """
 
-    def __init__(self,data_dir, sample):
+    def __init__(self,data_dir, sample = False):
         
         """
         Attributes:
@@ -174,17 +174,3 @@ class SGD:
         self.best_score = loss[idx]
         self.best_params = self.params[idx]
         self.fit(data,lr = self.best_params[1], reg = self.best_params[2], rank = self.best_params[3], num_epoch = self.best_params[4])        
-
-
-
-if __name__ == '__main__':
-
-    func = SGD('../data/ratings.csv',sample = True)
-    trainset, testset = func.split(0.25)
-    func.gridParams(num_epoch = [10,20], reg = [0.2,0.4], lr = [0.001,0.005], rank = [10,20])
-    func.tuningParams(trainset, verbose = True)
-    #func.fit(trainset)
-    #print(func.err(trainset, measure = 'rmse'))
-    print("Best learning rate: {}; Best lambda: {}; Best rank: {}; Best number of epoch: {}".format(func.best_params[1],func.best_params[2], func.best_params[3],func.best_params[4]))
-
-
